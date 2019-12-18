a15e33ab14f9c081a911d00b019eef133ceef4f2
ORKEMEY <p.artem.p.15@gmail.com>
2019-12-18 19:06:52 +0200

    Added button 'clear', added eslint, prettier, hasky

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: false,
+  },
+  extends: ['airbnb'],
+  plugins: ['import', 'prettier'],
+  parser: 'babel-eslint',
+  rules: {
+    'linebreak-style': 'off',
+    camelcase: 'off',
+    'no-unused-vars': 'off',
+    'no-continue': 'off',
+    'no-plusplus': 'off',
+    'no-param-reassign': 'off',
+    'prettier/prettier': ['error'],
+    'no-undef': 'off',
+    'no-arrow-parens': 'off',
+    'no-console': 'off',
+    'consistent-return': 'off',
+    'consistent-return': 'off',
+    'no-shadow': 'off',
+    'no-shadow': 'off',
+    'prettier/prettier': 'off'
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,96 +1,78 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+const express = require('express');
 
-const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://localhost";
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost';
 
 server.listen(3000);
 
-app.get('/', function(request, response){
-
-response.sendFile(__dirname + '/index.html');
-
+app.get('/', (request, response) => {
+  response.sendFile(`${__dirname}/index.html`);
 });
 
-Users = [];
 ConnectedUsersSockets = [];
 
-io.sockets.on('connection', function(socket){
+io.sockets.on('connection', (socket) => {
+  console.log(`Connected ${socket.id}`);
+  ConnectedUsersSockets.push(socket);
 
-    console.log("Connected");
-    ConnectedUsersSockets.push(socket);
+  socket.on('disconnect', (data) => {
+    ConnectedUsersSockets.splice(ConnectedUsersSockets.indexOf(socket), 1);
+    console.log(`Disconnected ${socket.id}`);
+  });
 
-    socket.on('disconnect', function (data) {
-        ConnectedUsersSockets.splice(ConnectedUsersSockets.indexOf(socket), 1);
-        console.log("Disconnected");
+  socket.on('send_message', (data) => {
+    io.sockets.emit('add_message', { message: data.message, name: data.name });
+
+    const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
+
+    mongoClient.connect((err, client) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      const db = client.db('usersdb');
+      const collection = db.collection('messages');
+      const user = { name: data.name, message: data.message };
+
+      collection.insertOne(user, (err, result) => {
+        if (err) return console.log(err);
+
+        console.log(result);
+        client.close();
+      });
     });
+  });
 
-    socket.on('send_message', function(data){
+  socket.on('history', () => {
+    const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
 
-        io.sockets.emit('add_message', { message: data.message, name: data.name });
+    mongoClient.connect((err, client) => {
+      if (err) {
+        return console.log(err);
+      }
 
-        const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
+      const db = client.db('usersdb');
+      const collection = db.collection('messages');
 
-        mongoClient.connect(function(err, client){
+      collection.find().toArray((err, results) => {
+        if (err) {
+          return console.log(err);
+        }
 
-            if(err){ return console.log(err); }
-            
-            const db = client.db("usersdb");
-            const collection = db.collection("messages");
-            let user = {name: data.name, message: data.message};
+        socket.emit('clear');
 
-            collection.insertOne(user, function(err, result){
-            if(err) return console.log(err);
-                 
-                console.log(result);
-                client.close();
-            });
-            
+        results.forEach((element) => {
+          socket.emit('add_message', { message: element.message, name: element.name });
         });
 
+        console.log(results);
+        client.close();
+      });
     });
-
-   socket.on('history', function(){
-
-        const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
-
-        mongoClient.connect(function(err, client){
-
-            if(err){ return console.log(err); }
-            
-            const db = client.db("usersdb");
-            const collection = db.collection("messages");
-
-            collection.find().toArray(function(err, results){
-
-                if(err) { return console.log(err); }
-
-               // io.sockets.emit('clear');
-
-                results.forEach(element => {
-                    
-                    io.sockets.emit('add_message',{message: element.message, name: element.name});
-                    //$textarea.append(data.name + ": " + data.message + "\n");
-                });
-
-                console.log(results);
-                client.close();
-            })
-            
-        });
-
-    });
-
-    socket.on('clear_history', function(data){
-        io.sockets.emit('clear');
-    });
-
-
+  });
 });
-
-
-
-
-
